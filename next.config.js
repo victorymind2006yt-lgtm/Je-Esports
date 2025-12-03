9bb59c530f29dfa281ea12039ec2387e02b3081b
Fazal <xml786786mr@gmail.com>
2025-12-03 15:22:25 +0500

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "anyimage.io",
+        hostname: "**.anyimage.io",
       },
     ],
   },
 };
 
-// updated logo config
 module.exports = nextConfig;
