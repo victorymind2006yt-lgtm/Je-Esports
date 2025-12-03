529fa6063360b688289f2b458b655fcb6b2d4c82
Fazal <xml786786mr@gmail.com>
2025-12-03 06:43:10 +0500

    Fixed Next.js config - removed invalid experimental options

--- next.config.js
+++ next.config.js
@@ -8,17 +8,6 @@ const nextConfig = {
       },
     ],
   },
-  // Increase memory limit for the build process
-  experimental: {
-    turbo: {
-      // Disable Turbopack if it's causing issues
-      loaders: {
-        // Adjust loaders if needed
-      },
-    },
-    // Increase build memory limit
-    memoryLimit: 8 * 1024, // 8GB
-  },
 };
 
 module.exports = nextConfig;
